65f006a84ee76efb7a7b377e740f939d0258ab57
Jiří Bednář <jbednar@protonmail.ch>
2021-07-16 14:07:52 +1000

    Changed C11 mutex to SDL mutex for portability

--- src/squeaker.c
+++ src/squeaker.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <threads.h>
 #include <math.h>
 #include <SDL2/SDL.h>
+#include <SDL2/SDL_mutex.h>
 
 #define SAMPLE_RATE 44100
 #define TAU 6.28318530717958647692528676655900576839433879875 /* 2*PI */
@@ -10,7 +10,7 @@ typedef struct
 {
   double frequency;
   double phase;
-  mtx_t *mtx;
+  SDL_mutex *mtx;
 } tone;
 
 void fill_audio(void *udata, Uint8 *stream, int len)
@@ -21,9 +21,9 @@ void fill_audio(void *udata, Uint8 *stream, int len)
     /* sine wave fitted into Uint8 */
     stream[i]=round((sin(tone->phase)+1)*127); 
     /* Time step */
-    mtx_lock(tone->mtx);
+    SDL_LockMutex(tone->mtx);
     tone->phase += (TAU/SAMPLE_RATE) * tone->frequency;
-    mtx_unlock(tone->mtx);
+    SDL_UnlockMutex(tone->mtx);
   }
   /* Wrap phase to stop it from growing uncontrollably */
   tone->phase = fmod(tone->phase,TAU);
@@ -31,13 +31,13 @@ void fill_audio(void *udata, Uint8 *stream, int len)
 
 int main()
 {
-  mtx_t mtx;
-  if(mtx_init(&mtx,mtx_plain) == thrd_error)
+  SDL_mutex *mtx;
+  if(!(mtx = SDL_CreateMutex()))
   {
     fprintf(stderr, "Couldn't initialise mutex\n");
     return(-1);
   }
-  tone tone = {.frequency = 0.0, .phase = 0.0, .mtx = &mtx};
+  tone tone = {.frequency = 0.0, .phase = 0.0, .mtx = mtx};
   SDL_AudioSpec audio;
   audio.freq = SAMPLE_RATE;
   audio.format = AUDIO_U8; /* Unsigned 8-bit int */
@@ -62,13 +62,13 @@ int main()
     };
     if(tone.frequency != new_freq)
     {
-      mtx_lock(&mtx);
+      SDL_LockMutex(mtx);
       tone.frequency = new_freq;
-      mtx_unlock(&mtx);
+      SDL_UnlockMutex(mtx);
     }
   }
   /* scanf didn't detect float, quitting */
   SDL_CloseAudio();
-  mtx_destroy(&mtx);
+  SDL_DestroyMutex(mtx);
   return(0);
 }
