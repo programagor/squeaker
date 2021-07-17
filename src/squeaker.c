263bf1c4aafda53f6e773df98febd079674faafb
Jiří Bednář <jbednar@protonmail.ch>
2021-07-17 11:14:02 +1000

    Added portamento feature

--- src/squeaker.c
+++ src/squeaker.c
@@ -4,13 +4,16 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mutex.h>
 
-#define SAMPLE_RATE 44100
 #define TAU 6.28318530717958647692528676655900576839433879875 /* 2*PI */
+#define SAMPLE_RATE 44100
+#define PORTAMENTO 0.1
 
 typedef struct
 {
-  double frequency;
+  double freq;
+  double new_freq;
   double phase;
+  double portamento;
   SDL_mutex *mtx;
 } tone;
 
@@ -19,28 +22,59 @@ void fill_audio(void *udata, Uint8 *stream, int len)
   tone *tone = udata;
   for(int i=0; i<len; i++)
   {
-    /* sine wave fitted into Uint8 */
-    stream[i]=round((sin(tone->phase)+1)*127);
     /* Time step */
     SDL_LockMutex(tone->mtx);
-    tone->phase += (TAU/SAMPLE_RATE) * tone->frequency;
+    {
+      /* sine wave fitted into Uint8 */
+      stream[i]=round((sin(tone->phase)+1)*127);
+      /* Update frequency */
+      if(tone->portamento)
+      {
+        /* Slide from current frequency to new frequency.
+         * This implements simple P controller, with asymptotic behaviour.
+         */
+        tone->freq+=(tone->new_freq-tone->freq)/(SAMPLE_RATE*tone->portamento);
+      }
+      else
+      {
+        /* If portamento is 0, jump straight to new frequency */
+        tone->freq = tone->new_freq;
+      }
+      /* Update phase */
+      tone->phase += (TAU/SAMPLE_RATE) * tone->freq;
+    }
     SDL_UnlockMutex(tone->mtx);
   }
   /* Wrap phase to stop it from growing uncontrollably */
   tone->phase = fmod(tone->phase,TAU);
 }
 
-int main()
+int main(int argc, const char *argv[])
 {
   SDL_SetMainReady();
-  SDL_Init(SDL_INIT_AUDIO);
+  if(SDL_Init(SDL_INIT_AUDIO) != 0)
+  {
+    fprintf(stderr,"Unable to initialize SDL: %s", SDL_GetError());
+    return(-1);
+  }
   SDL_mutex *mtx;
   if(!(mtx = SDL_CreateMutex()))
   {
     fprintf(stderr, "Couldn't initialise mutex\n");
     return(-1);
   }
-  tone tone = {.frequency = 0.0, .phase = 0.0, .mtx = mtx};
+  tone tone = 
+  {
+    .freq = 0.0,
+    .new_freq = 0.0,
+    .phase = 0.0,
+    .portamento = PORTAMENTO,
+    .mtx = mtx
+  };
+  if(argc==2)
+  {
+    sscanf(argv[1],"%lf",&(tone.portamento));
+  }
   SDL_AudioSpec audio;
   audio.freq = SAMPLE_RATE;
   audio.format = AUDIO_U8; /* Unsigned 8-bit int */
@@ -48,7 +82,7 @@ int main()
   audio.samples = 1024;
   audio.callback = fill_audio;
   audio.userdata = &tone;
-  if(SDL_OpenAudio(&audio, NULL) < 0)
+  if(SDL_OpenAudio(&audio, NULL) != 0)
   {
     fprintf(stderr, "Couldn't open audio: %s\n", SDL_GetError());
     return(-1);
@@ -63,15 +97,18 @@ int main()
     if(!scanf("%lf",&new_freq)){
       continuing = 0;
     };
-    if(tone.frequency != new_freq)
+    if(tone.new_freq != new_freq)
     {
       SDL_LockMutex(mtx);
-      tone.frequency = new_freq;
+      {
+        tone.new_freq = new_freq;
+      }
       SDL_UnlockMutex(mtx);
     }
   }
   /* scanf didn't detect float, quitting */
   SDL_CloseAudio();
   SDL_DestroyMutex(mtx);
+  SDL_Quit();
   return(0);
 }
