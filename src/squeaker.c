1499184fff99167ca331b72667d1f35ada088678
Jiří Bednář <jbednar@protonmail.ch>
2021-07-20 11:37:56 +1000

    Increased minimum frequency to avoid slow rampups

--- src/squeaker.c
+++ src/squeaker.c
@@ -7,7 +7,8 @@
 #define TAU 6.28318530717958647692528676655900576839433879875 /* 2*PI */
 #define SAMPLE_RATE 44100
 #define INPUT_LOWPASS 1e-2
-#define MIN_FREQ 1e-4
+#define MIN_INPUT_LOWPASS 1e-2
+#define MIN_FREQ 10
 
 typedef struct
 {
@@ -31,7 +32,7 @@ void fill_audio(void *udata, Uint8 *stream, int len)
       /* written byte by byte */
       for(int j=0; j<4; j++) stream[i+j] = ((Uint8*) &sample)[j];
       /* Update frequency */
-      if(tone->input_lowpass >= MIN_FREQ)
+      if(tone->input_lowpass >= MIN_INPUT_LOWPASS)
       {
         /* Slide from current frequency to new frequency.
          * This implements simple P controller, with asymptotic behaviour.
